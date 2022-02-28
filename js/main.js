b22e10fa02b32204eee4869a6e99bdff65c60c6f
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-28 03:46:22 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+   function compOpen() {
+ document.getElementById("footer_company").style.display = "block";
+}
+function compClose() {
+ document.getElementById("footer_company").style.display = "none";
+}
